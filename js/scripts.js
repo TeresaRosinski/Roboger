422b68d93382c9c2a3dd83e2f6b0e60c8174f71b
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-06-12 16:30:05 -0500

    Add a function to get an array of numbers from 0 to the users input

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,57 @@
 //Business Logic
+function getNumbersToInput(number){
+  let orderedNumberArray = [];  // function to create array of numbers from 0 - user input
+  for (let i = 0; i<= number; i++)
+  orderedNumberArray.push(i);
+  console.log(orderedNumberArray);
+  return orderedNumberArray;
+};
 function NumberTranslator (number) {
   let displayedOutput= " ";
-  /*const userNumberArray = number.split('');
+  let orderedNumberArray = []; //to get all the numbers from 0 to inputted number
+  const textAndNumberArray = []; // final array  with all ints + words from 0 to user number
+  function getNumbersToInput(number){
+    let orderedNumberArray = [];  // function to create array of numbers from 0 - user input
+    for (let i = 0; i<= number.length; i++)
+    orderedNumberArray.push(i);
+  };
+  /*
+  const output = numberArray.map(number=>{ //
+    ApplyRules(number)
+  });
+  
+  for (const digit of number){ 
+    debugger;
+    if (digit === "3"){
+      displayedOutput = displayedOutput.concat("Won't you be my neighbor");
+      return displayedOutput; 
+    } else  if (digit === "2"){
+      displayedOutput = displayedOutput.concat("Boop!");
+      return displayedOutput;
+    }else if (digit === "1") {
+      displayedOutput = displayedOutput.concat("Beep!");
+      return displayedOutput;
+    } else return digit;
+    
+  };
+  */
+};
+
+//UI Logic
+$(document).ready(function(){
+  $("#inputNumberForm").submit(function(event){
+    const numberInput =$("input#number").val();
+    const uIResultsToShow = getNumbersToInput(numberInput);
+
+  $("#results").show();
+  $(".yourNumber").empty().append(uIResultsToShow);
+  event.preventDefault();
+  });
+});
+
+
+//Old Code that may be useful
+ /*const userNumberArray = number.split('');
   console.log(userNumberArray);
   if (number === NaN || number < 0) {
     displayedOutput+= "Please input a whole number greater than 0";
@@ -19,30 +69,3 @@ function NumberTranslator (number) {
     }
   });
   */
-  
-  for (const digit of number){
-    debugger;
-    if (digit === "3"){
-      displayedOutput = displayedOutput.concat("Won't you be my neighbor");
-      return displayedOutput; 
-    } else  if (digit === "2"){
-      displayedOutput = displayedOutput.concat("Boop!");
-      return displayedOutput;
-    }else if (digit === "1") {
-      displayedOutput = displayedOutput.concat("Beep!");
-      return displayedOutput;
-    }
-  };
-};
-
-//UI Logic
-$(document).ready(function(){
-  $("#inputNumberForm").submit(function(event){
-    const numberInput =$("input#number").val();
-    const uIResultsToShow = NumberTranslator(numberInput);
-
-  $("#results").show();
-  $(".yourNumber").empty().append(uIResultsToShow);
-  event.preventDefault();
-  });
-});
