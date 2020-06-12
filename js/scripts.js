9b0409e823173a685d8a8d9b4fa5256f8e58c682
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-06-12 17:34:37 -0500

    add another failed but closer hopefully attempt at creating this function

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,53 @@
 //Business Logic
-function getNumbersToInput(number){
-  let orderedNumberArray = [];  // function to create array of numbers from 0 - user input
-  for (let i = 0; i<= number; i++)
-  orderedNumberArray.push(i);
-  console.log(orderedNumberArray);
-  return orderedNumberArray;
-};
+
 function NumberTranslator (number) {
-  let displayedOutput= " ";
-  let orderedNumberArray = []; //to get all the numbers from 0 to inputted number
-  const textAndNumberArray = []; // final array  with all ints + words from 0 to user number
-  function getNumbersToInput(number){
+  let orderedNumberArray = createOrderedNumberArray(number); //to get all the numbers from 0 to inputted number
+  let outputArray= createOutputArray(orderedNumberArray);
+  //const textAndNumberArray = []; // final array  with all ints + words from 0 to user number
+  
+  function createOrderedNumberArray(number){
     let orderedNumberArray = [];  // function to create array of numbers from 0 - user input
-    for (let i = 0; i<= number.length; i++)
+    for (let i = 0; i<= number; i++)
     orderedNumberArray.push(i);
+    return orderedNumberArray;
+  };
+  debugger;
+
+  function createOutputArray (orderedNumberArray) {
+    let outputArray = [];
+      orderedNumberArray.forEach(function(element){
+        for (const digit of element){ 
+          if (digit === "3"){
+            return "Won't you be my neighbor";
+          } else  if (digit === "2"){
+            return "Boop!";
+          }else if (digit === "1") {
+            return "Beep!"
+          } else return digit;
+      };
+      });
+
   };
   /*
-  const output = numberArray.map(number=>{ //
-    ApplyRules(number)
-  });
-  
-  for (const digit of number){ 
-    debugger;
-    if (digit === "3"){
-      displayedOutput = displayedOutput.concat("Won't you be my neighbor");
-      return displayedOutput; 
-    } else  if (digit === "2"){
-      displayedOutput = displayedOutput.concat("Boop!");
-      return displayedOutput;
-    }else if (digit === "1") {
-      displayedOutput = displayedOutput.concat("Beep!");
-      return displayedOutput;
-    } else return digit;
-    
+  outputArray = orderedNumberArray.map(function(element){ 
+    for (const digit of element){ 
+      if (digit === "3"){
+        return "Won't you be my neighbor";
+      } else  if (digit === "2"){
+        return "Boop!";
+      }else if (digit === "1") {
+        return "Beep!"
+      } else return digit;
   };
   */
+
 };
 
 //UI Logic
 $(document).ready(function(){
   $("#inputNumberForm").submit(function(event){
     const numberInput =$("input#number").val();
-    const uIResultsToShow = getNumbersToInput(numberInput);
+    const uIResultsToShow = NumberTranslator(numberInput);
 
   $("#results").show();
   $(".yourNumber").empty().append(uIResultsToShow);
@@ -50,22 +56,3 @@ $(document).ready(function(){
 });
 
 
-//Old Code that may be useful
- /*const userNumberArray = number.split('');
-  console.log(userNumberArray);
-  if (number === NaN || number < 0) {
-    displayedOutput+= "Please input a whole number greater than 0";
-  }
-  userNumber.forEach(function(numeral){
-    if(numeral === 3){
-      displayedOutput = displayedOutput.concat("Won't you be my neighbor");
-      displayedOutput; 
-    } else  if (numeral === 2){
-      displayedOutput = displayedOutput.concat("Boop!");
-      return displayedOutput;
-    }else if (numeral === 1) {
-      displayedOutput = displayedOutput.concat("Beep!");
-      return displayedOutput;
-    }
-  });
-  */
