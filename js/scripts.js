0e74903b57682dabc1cad56977a7ec61b64b75c4
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-06-12 16:13:26 -0500

    Add variety of attempted yet failed functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
 //Business Logic
 function NumberTranslator (number) {
   let displayedOutput= " ";
+  /*const userNumberArray = number.split('');
+  console.log(userNumberArray);
   if (number === NaN || number < 0) {
     displayedOutput+= "Please input a whole number greater than 0";
-  } 
+  }
+  userNumber.forEach(function(numeral){
+    if(numeral === 3){
+      displayedOutput = displayedOutput.concat("Won't you be my neighbor");
+      displayedOutput; 
+    } else  if (numeral === 2){
+      displayedOutput = displayedOutput.concat("Boop!");
+      return displayedOutput;
+    }else if (numeral === 1) {
+      displayedOutput = displayedOutput.concat("Beep!");
+      return displayedOutput;
+    }
+  });
+  */
+  
   for (const digit of number){
+    debugger;
     if (digit === "3"){
       displayedOutput = displayedOutput.concat("Won't you be my neighbor");
       return displayedOutput; 
@@ -15,8 +32,8 @@ function NumberTranslator (number) {
       displayedOutput = displayedOutput.concat("Beep!");
       return displayedOutput;
     }
-  }
-}
+  };
+};
 
 //UI Logic
 $(document).ready(function(){
