0f76b089723729d1b809cc4c7dcb2ba88387b0a2
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-06-12 15:10:39 -0500

    function will display the three phrases but it only displays them based on the fist number in the string

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,31 @@
 //Business Logic
 function NumberTranslator (number) {
+  let displayedOutput= " ";
   if (number === NaN || number < 0) {
-    alert ("Please input a whole number greater than 0")
-    
+    displayedOutput+= "Please input a whole number greater than 0";
+  } 
+  for (const digit of number){
+    if (digit === "3"){
+      displayedOutput = displayedOutput.concat("Won't you be my neighbor");
+      return displayedOutput; 
+    } else  if (digit === "2"){
+      displayedOutput = displayedOutput.concat("Boop!");
+      return displayedOutput;
+    }else if (digit === "1") {
+      displayedOutput = displayedOutput.concat("Beep!");
+      return displayedOutput;
+    }
   }
-
-
 }
 
 //UI Logic
 $(document).ready(function(){
   $("#inputNumberForm").submit(function(event){
     const numberInput =$("input#number").val();
-    const results = NumberTranslator(numberInput);
+    const uIResultsToShow = NumberTranslator(numberInput);
 
   $("#results").show();
-  $(".yourNumber").empty().append(numberInput);
+  $(".yourNumber").empty().append(uIResultsToShow);
   event.preventDefault();
   });
 });
