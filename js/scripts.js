c1f0a6eee9a3f8019698bdc4a6db51408b87108a
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-06-12 14:56:26 -0500

    function will not accept a negative number

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 //Business Logic
+function NumberTranslator (number) {
+  if (number === NaN || number < 0) {
+    alert ("Please input a whole number greater than 0")
+    
+  }
+
+
+}
 
 //UI Logic
+$(document).ready(function(){
+  $("#inputNumberForm").submit(function(event){
+    const numberInput =$("input#number").val();
+    const results = NumberTranslator(numberInput);
+
+  $("#results").show();
+  $(".yourNumber").empty().append(numberInput);
+  event.preventDefault();
+  });
+});
